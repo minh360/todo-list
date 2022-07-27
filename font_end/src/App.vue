36bcf90e3c9f88456c63fc8f68a1253f42a2a1e5
minh360 <minhtrai23112001@gmail.com>
2022-07-27 16:50:41 +0700

    Initial commit

---
+++ font_end/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+  <footer-panel />
+</template>
+
+<script setup>
+import FooterPanel from "@/components/Layouts/FooterPanel";
+</script>
+
+<style lang="scss">
+#app{
+  background-image: url('../public/back_ground.jpg');
+  background-size: 100% 100vh;
+}
+</style>
---
+++ font_end/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import axios from 'axios';
+const todos = ref([])
+const fetchTodo = async function () {
+  await axios.get("http://localhost:3000/todo").then(response => {
+    todos.value = response.data;
+  });
+}
+onMounted(() =>
+  fetchTodo()
+)
+</script>
+
+<template>
+  <div v-show="todos.length>0">
+    {{todos}}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
